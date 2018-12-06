38e78eb1d5fb5b682f62d904de7aa72305fd0e8c
admin <1054958043@qq.com>
2018-12-06 21:07:35 +0800

    本次修改

--- git/src/com/itheima/git/Demo.java
+++ git/src/com/itheima/git/Demo.java
@@ -1,5 +1,7 @@
 package com.itheima.git;
 
+import javax.sound.midi.Soundbank;
+
 public class Demo {
     public static void main(String[] args) {
         System.out.println("hello git");
@@ -8,10 +10,12 @@ public class Demo {
         System.out.println("hello git");
         System.out.println("hello git");
         System.out.println("hello git");
+        System.out.println("===================");
     }
 
     public  void test(){
         int i = 0;
         i += 10;
+        System.out.println(i);
     }
 }
