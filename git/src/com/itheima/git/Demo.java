3f51b68fafd05723089caacd5193233f7f3c65e0
admin <1054958043@qq.com>
2018-12-06 20:55:02 +0800

    把本地工程添加到git

---
+++ git/src/com/itheima/git/Demo.java
@@ -0,0 +1,12 @@
+package com.itheima.git;
+
+public class Demo {
+    public static void main(String[] args) {
+        System.out.println("hello git");
+        System.out.println("hello git");
+        System.out.println("hello git");
+        System.out.println("hello git");
+        System.out.println("hello git");
+        System.out.println("hello git");
+    }
+}
