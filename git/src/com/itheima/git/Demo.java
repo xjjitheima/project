feb3d6111787aa7fa33dc6de2c78ad22213dc4f9
admin <1054958043@qq.com>
2018-12-06 20:57:04 +0800

    再次把修改的本地工程添加到git

--- git/src/com/itheima/git/Demo.java
+++ git/src/com/itheima/git/Demo.java
@@ -9,4 +9,9 @@ public class Demo {
         System.out.println("hello git");
         System.out.println("hello git");
     }
+
+    public  void test(){
+        int i = 0;
+        i += 10;
+    }
 }
